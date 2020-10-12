5945a06c3a02e6c6295af7707a567f5b37805266
Yevhen Triukhan <yevhen.triukhan@gmail.com>
2020-10-12 13:24:14 +0300

    Cleaning.

--- EasyGitHubSearch/Source/Services/Networking/Networking+SearchUsers.swift
+++ EasyGitHubSearch/Source/Services/Networking/Networking+SearchUsers.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+extension Networking {
+    
+    public func search(users query: String, page: Int, completion: @escaping Handler<Result<SearchUserResponse, Error>>) {
+        
+        let queryItems = [
+            URLQueryItem(name: "q", value: query),
+            URLQueryItem(name: "page", value: page.string),
+            URLQueryItem(name: "per_page", value: Constant.perPage.string)
+        ]
+        let request = self.createRequest(method: .get,
+                                         queryItems: queryItems,
+                                         path: "/search/users")
+        
+        return self.send(request: request, completion: completion)
+    }
+}
