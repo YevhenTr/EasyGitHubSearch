402422eaf1e44a686296edf8b60704b2b58bb66a
Yevhen Triukhan <yevhen.triukhan@gmail.com>
2020-10-12 13:28:03 +0300

    UserListViewController -> initial commit.

--- EasyGitHubSearch/EasyGitHubSearch/AppDelegate.swift
+++ EasyGitHubSearch/EasyGitHubSearch/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = ViewController(nibName: nil, bundle: nil)
+        window.rootViewController = UserListViewController(nibName: nil, bundle: nil)
         window.makeKeyAndVisible()
         
         self.window = window
---
+++ EasyGitHubSearch/Source/UI/UserListViewController/UserListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UserListViewController.swift
+//  EasyGitHubSearch
+//
+//  Created by Yevhen Triukhan on 12.10.2020.
+//  Copyright © 2020 Yevhen Triukhan. All rights reserved.
+//
+
+import UIKit
+
+class UserListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
